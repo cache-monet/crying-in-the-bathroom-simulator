47499bef363206314c0da896ede24b484b74bc31
remi <wjpan@uwaterloo.ca>
2021-02-07 22:24:32 -0800

    serverless

--- music_processor.py
+++ music_processor.py
@@ -1,17 +1,20 @@
 import youtube_dl
 from pysndfx import AudioEffectsChain
+import os
 
 class MusicProcessor:
   def __init__(self, outdir):
     self.outdir =  outdir
     self.ydl = youtube_dl.YoutubeDL({
       'format': 'bestaudio/best',
+      'cachedir': '/tmp/',
+      'ffmpeg_location': '/opt/ffmpeg',
       'postprocessors': [{
           'key': 'FFmpegExtractAudio',
           'preferredcodec': 'mp3',
           'preferredquality': '128',
       }],
-      'outtmpl': '{}/%(id)s_source.%(ext)s'.format(self.outdir),
+      'outtmpl': '{}/%(id)s.%(ext)s'.format(self.outdir),
     })
 
     self.fx = (
@@ -25,7 +28,7 @@ class MusicProcessor:
   def process(self, url: str):
     info_dict = self.ydl.extract_info(url, download=True)
     fid = info_dict.get('id')
-    src = "{}/{}_source.mp3".format(self.outdir, fid)
-    dst = "{}/{}.mp3".format(self.outdir, fid)
-    self.fx(src, dst)
+    src = f"{self.outdir}/{fid}.mp3"
+    dst = f"{self.outdir}/out.mp3"
+    self.fx(src=src, dst=dst)
     return dst, "{}.mp3".format(fid)
